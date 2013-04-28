908aa6a6113e376bf835ce95a8d7304b098e1ad7
unknown <MM@N22132D1.(none)>
2013-04-28 16:38:41 +0800

    New add database

---
+++ src/com/webin/core/db/WebinRobot.java
@@ -0,0 +1,17 @@
+package com.webin.core.db;
+
+public class WebinRobot {
+	private String _id;
+	private String _code;
+	private String _weight;
+	private String _reply0;
+	private String _reply1;
+	private String _reply2;
+	private String _reply3;
+	private String _reply4;
+	private String _reply5;
+	private String _reply6;
+	private String _reply7;
+	private String _reply8;
+	private String _reply9;
+}
--- src/com/webin/core/script/ChatMenu.java
+++ src/com/webin/core/script/ChatMenu.java
@@ -4,7 +4,7 @@ import java.io.PrintWriter;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.webin.core.robot.RobotDatabase;
+import com.webin.core.db.RobotDatabase;
 import com.webin.core.wechat.MsgTag;
 import com.webin.core.wechat.TextMsg;
 
@@ -16,7 +16,42 @@ public class ChatMenu implements IHandle {
 		replay.setContent(msg);
 		writer.print(replay.toXML());
 	}
+	
+	private void menuUpdate(MsgTag tag, PrintWriter writer){
+		String content = "谢谢提交已收录";
+		menuNormal(tag, writer, content);
+	}
+	
+	private void handleOtherWord(MsgTag tag, PrintWriter writer) {
+		String content = "然后呢";
+		menuNormal(tag, writer, content);
+	}
+	
+	private boolean isHandleRecorder(MsgTag tag, PrintWriter writer){
+		String[] words = tag.Content.split(",");
+		if (words.length == 1) {
+			words = tag.Content.split("，");
+		}
+		if (words.length == 3 && words[0].equals("收录")){
+			ResultSet result = mRobotDatabase.executeQuery(words[1]);
+			try {
+				if (result.first()){
+					mRobotDatabase.executeUpdate(words[1], words[2]);
+				}else{
+					mRobotDatabase.executeInsert(words[1], words[2]);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			menuUpdate(tag, writer);
+			return true;
+		}
+		return false;
+	}
 
+	/*
+	 * WebinRobot _id _code _weight _reply0
+	 */
 	private boolean isHandleByRobot(MsgTag tag, PrintWriter writer) {
 		ResultSet result = mRobotDatabase.executeQuery(tag.Content);
 		try {
@@ -32,13 +67,11 @@ public class ChatMenu implements IHandle {
 		return false;
 	}
 
-	private void handleOtherWord(MsgTag tag, PrintWriter writer) {
-		String content = "然后呢";
-		menuNormal(tag, writer, content);
-	}
-
 	@Override
 	public void handleMsg(MsgTag tag, PrintWriter writer) {
+		if (isHandleRecorder(tag, writer)){
+			return;
+		}
 		if (!isHandleByRobot(tag, writer)){
 			handleOtherWord(tag, writer);
 		}
